ab916f6e92130ecc20d820629e702acb45ced836
vidsdn <98356493+vidsdn@users.noreply.github.com>
2022-02-05 16:00:36 -0600

    Added Import for @JsonIgnore

--- src/main/java/com/ninja/lms/entity/Program.java
+++ src/main/java/com/ninja/lms/entity/Program.java
@@ -12,6 +12,8 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 
 @Entity
 @Table(name="TBL_LMS_PROGRAM")
